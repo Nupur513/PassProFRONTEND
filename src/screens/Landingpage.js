a0ef05c24e6233abab852c255200625bbd07744c
Nupur513 <128990611+Nupur513@users.noreply.github.com>
2024-06-29 17:13:05 +0530

    landing page routing changed

--- src/screens/Landingpage.js
+++ src/screens/Landingpage.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Landingpage.css'; // Import custom CSS for landing page styling
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import Footer from '../components/Footer/Footer'
 
 const LandingPage = () => {
   return (
@@ -12,8 +12,12 @@ const LandingPage = () => {
         <p className="lead">Manage your outpasses with ease.</p>
 
         <div className="button-container">
-          <Button variant="primary" size="lg" href="/login">Login</Button>
-          <Button variant="outline-primary" size="lg" href="/register">Register</Button>
+          <Link to="/login">
+            <Button variant="primary" size="lg">Login</Button>
+          </Link>
+          <Link to="/register">
+            <Button variant="outline-primary" size="lg">Register</Button>
+          </Link>
         </div>
       </Container>
     </div>
@@ -21,7 +25,5 @@ const LandingPage = () => {
 }
 
 export default LandingPage;
-
-
 
 
