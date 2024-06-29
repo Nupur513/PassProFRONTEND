4e351e3221e33b9c898aea49df755a8a422569db
Nupur513 <128990611+Nupur513@users.noreply.github.com>
2024-06-29 14:33:35 +0530

    Configured Axios to use environment variable for API URL

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3500/api', // Adjust the URL as per your backend setup
+  baseURL: process.env.REACT_APP_API_URL, // Adjust the URL as per your backend setup
   headers: {
     'Content-Type': 'application/json',
   },
